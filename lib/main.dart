17857f4da2d78043bbf3a0e77abb2c9019e65da6
xiaoqiyang <cyou_yang@163.com>
2022-04-29 16:43:52 +0800

    初始化

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dating_template/utils/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,18 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '社区交友APP',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: WcaoTheme.primary,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
---
+++ lib/utils/theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class WcaoTheme {
+  static Color primary = const Color(0xff8769fa);
+}
